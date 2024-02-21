32934c7fa9e07574fcbfde7b3bb2f7dfb1e36af8
joseph6655 <josephsassine66@gmail.com>
2024-02-21 16:36:35 +0200

    new changes

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -1,29 +1,44 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Footer = () => {
   return (
-    
-      <footer>
-        <div class="footerall">
-            <img src="/creoshiftlogo.jpg" alt="Footer Logo"/>
-        
-        <div class="footer-links">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+    <footer>
+      <div className="footerall">
+        <img src="/creoshiftlogo.jpg" alt="Footer Logo" />
+        <div className="footer-links">
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
-        <div class="social-icons">
-            <Link href="https://www.Facebook.com" class="Facebook-icon" img src="public/facebooklogo.png"Link/>
-            < Link href="https://www.Whatsup.com" class="whats-up-icon" img src="/whatsuplogo.png"Link/>
-            < link href="https://www.Instagram.com" class="Instagram-icon"img src="joseph/public/instagramlogo.jpg" Link/>
+        <div className="social-icons">
+          <Link href="https://www.Facebook.com"
+             className="Facebook-icon">
+              <img src="/facebooklogo.png" alt="Facebook" />
+            
+          </Link>
+          <Link href="https://www.Whatsup.com"
+            className="whats-up-icon">
+              <img src="/whatsuplogo.png" alt="WhatsApp" />
+           
+          </Link>
+          <Link href="https://www.Instagram.com"
+             className="Instagram-icon">
+              <img src="/instagramlogo.jpg" alt="Instagram" />
+           
+          </Link>
         </div>
-        </div>
+      </div>
     </footer>
-    
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
